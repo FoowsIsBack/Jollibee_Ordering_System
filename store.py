debad38bf049439273c6eaba1444ae5942706961
Foows - (Dave) <dbangcoyo11@gmail.com>
2024-11-26 17:28:39 +0800

    Update store.py

--- store.py
+++ store.py
@@ -1,2 +1,16 @@
-import time
 import os
+import time
+
+def clear():
+    if os.name == 'nt':  
+        os.system('cls')
+    else:
+        os.system('clear')
+
+def main():
+    print("""   ┏━━━━━━━━━━━━━━━━━━━┓ 
+      Food Mini Store
+          """)
+
+
+main()
